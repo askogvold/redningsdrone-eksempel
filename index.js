70050bb9c3694e34b66f8c1bc9a36db825a8d1d3
Norsk Redningsdrone <post@redningsdrone.no>
2016-02-09 20:07:12 +0100

    Now sending meas messages from tailed file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var mode="production";
+
 var express = require('express');
 var WebSocketServer = require('ws').Server;
 var http = require('http');
+
+var FileTail = require('file-tail');
 
 var port = process.env.PORT || 5000;
 
@@ -34,7 +38,7 @@ var currentInfo = function (date) {
 
     var dronePos = [lat,lon];
 
-    return JSON.stringify({
+    return {
         drone: {
             pos: dronePos,
             time: date
@@ -45,7 +49,7 @@ var currentInfo = function (date) {
                 radius: 500,
                 time: date
             }]
-    });
+    };
 };
 
 var sockets = new Set();
@@ -60,8 +64,27 @@ wss.on('connection', function (ws) {
     })
 });
 
+if(mode !== "production") {
 setInterval(function () {
     sockets.forEach(function(ws) {
     ws.send(currentInfo(new Date()));
   });
 }, 1000);
+}
+
+// Start listening on meas messages
+
+
+var ft = FileTail.startTailing('/var/log/meas_json/current');
+
+ft.on('line', function(line) {
+  console.log('Received meas: ' + line);
+  var obj = JSON.parse(line);
+  var time = new Date(obj.time * 1000);
+  var msg = currentInfo(time);
+  msg.meas = obj;
+  sockets.forEach(function(ws) {
+    ws.send(JSON.stringify(msg));
+  });
+  
+});
